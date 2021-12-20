c7482b005cc1bd4c729b465be044092086950abf
Mohammed El Batya <elbatya@googlemail.com>
2021-12-20 20:03:28 +0100

    fast day14 solution

--- src/day_14.rs
+++ src/day_14.rs
@@ -1,9 +1,7 @@
 mod lib;
 
 use std::collections::HashMap;
-use std::fmt::{Debug};
-use std::time::Instant;
-use itertools::{Either, Itertools, MinMaxResult};
+use itertools::{Itertools, MinMaxResult};
 use rayon::prelude::*;
 use crate::lib::{read_lines};
 
@@ -11,13 +9,9 @@ fn main() {
     let result_a = task(read_lines("input/day_14.txt"), 10);
     assert_eq!(result_a, 2621);
 
-    for i in 20..30 {
-        let before = Instant::now();
-        task(read_lines("input/day_14.txt"), i);
-        println!("Elapsed time {}: {:.2?}", i, before.elapsed());
-    }
-
     let result_b = task(read_lines("input/day_14.txt"), 40);
+    assert_eq!(result_b, 2843834241366);
+
     println!("task-a: {}, task-b: {}", result_a, result_b);
 }
 
@@ -26,40 +20,65 @@ fn task(lines: impl Iterator<Item=String>, steps: usize) -> usize {
     let rule_map = rules.iter().map(|r|(r.0.clone(), r.clone())).collect::<HashMap<_,_>>();
     let seed = template.iter().tuple_windows().collect_vec().into_iter().map(|(a, b)|[*a, *b]);
     let mut stats = seed.collect_vec().par_iter()
-        .map(|seed| {
-            stream_apply(Box::new(vec![[seed[0], seed[1]]].into_iter()), &rule_map, steps)
-                .map(|pair|pair[0])
-                .fold(HashMap::new(), |mut stat,c| {
-                    *stat.entry(c).or_insert(0) += 1;
-                    stat
-                })
-    }).reduce_with(|mut a: HashMap<_, _>, b: HashMap<_, _>| {
-        b.iter().for_each(|(k,v)| *a.entry(*k).or_insert(0) += v);
-        a
-    }).unwrap();
+         .map(|seed| {
+             recursive_edit(seed, steps,&rule_map,&mut Cache::new())
+         }).reduce_with(|a: Stats, b: Stats| merge_stats(&a,&b))
+        .expect("reduce failed");
 
     *stats.entry(template[template.len()-1]).or_insert(0) += 1;
     score(&stats)
 }
 
-fn single_edit(pair: &[char; 2], rules: &HashMap<[char;2], Rule>) -> Vec<[char;2]>  {
+fn single_edit(pair: &[char; 2], rules: &Rules) -> Vec<Element>  {
     match rules.get(pair) {
         None => vec![pair.clone()],
         Some((_, insert)) => vec![[pair[0], *insert],[*insert, pair[1]]],
     }
 }
 
-fn stream_apply<'a>(pairs: Box<dyn Iterator<Item=[char;2]> + 'a>, rules: &'a HashMap<[char;2], Rule>, steps: usize)
-    -> Box<dyn Iterator<Item=[char;2]>+'a>{
-    let applied = Box::new(pairs
-        .map(move |pair|single_edit(&pair, &rules))
-        .flatten());
+fn recursive_edit(
+    element: &Element,
+    step: usize,
+    rules: &Rules,
+    cache: &mut Cache) -> Stats {
 
-    if steps-1 > 0 {
-        stream_apply(applied, &rules, steps-1)
+    let key = cache_key(element, &step);
+    if let Some(cached) = cache.get(&key){
+        cached.clone()
     } else {
-       applied
+        let stats = if step - 1 > 0  {
+            let edit = single_edit(&element, rules);
+            edit.iter()
+                .map(|e|recursive_edit(e, step-1, rules, cache))
+                .reduce(|a,b|merge_stats(&a, &b))
+                .expect("reduce failed")
+        } else {
+            let edit = single_edit(&element, rules);
+            stats(&edit)
+        };
+
+        cache.insert(key, stats.clone());
+        stats
     }
+}
+
+
+fn stats(elements: &Vec<Element>) -> Stats {
+    elements.iter().map(|pair|pair[0])
+        .fold(Stats::new(), |mut stat,c| {
+            *stat.entry(c).or_insert(0) += 1;
+            stat
+        })
+}
+
+fn merge_stats(a: &Stats, b: &Stats) -> Stats {
+    let mut m = a.clone();
+    b.iter().for_each(|(k,v)| *m.entry(*k).or_insert(0) += v);
+    m
+}
+
+fn cache_key(element: &Element, step: &usize) -> String {
+    format!("{}{}#{}", element[0], element[1], step)
 }
 
 
@@ -72,6 +91,11 @@ fn score(stat: &HashMap<char,usize>) -> usize {
 }
 
 type Rule = ([char;2], char);
+type Rules = HashMap<[char;2], Rule>;
+type Element = [char;2];
+type Stats = HashMap<char, usize>;
+type Cache = HashMap<String, Stats>;
+
 
 fn read_input(mut lines: impl Iterator<Item=String>) -> (Vec<char>, Vec<Rule>) {
     let template = lines.next().expect("invalid input").chars().collect_vec();
